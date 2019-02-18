e0ab8e0a3c9f2a425c8358ba20de953b3706175f
NomadBlacky <tkadowaki.dev.blackey@gmail.com>
2019-02-18 21:30:07 +0900

    wip

--- src/main/scala/Example.scala
+++ src/main/scala/Example.scala
@@ -1,11 +1,17 @@
 import net.logstash.logback.argument.StructuredArguments
+import net.logstash.logback.marker.Markers
 import org.slf4j.LoggerFactory
 
 object Example extends App {
 
   val logger = LoggerFactory.getLogger(getClass)
 
+  // ふつうのログ
   logger.info("Hello, Logging with JSON!")
 
+  // StructuredArgument で動的にフィールドを追加
   logger.info("log message {}", StructuredArguments.value("name", Map(1 -> "a")))
+
+  // Marker で動的にフィールドを追加
+  logger.info(Markers.append("marker", "mkr"), "Marker")
 }
