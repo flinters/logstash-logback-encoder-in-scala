6b0c3409ca9981f888ba8fe08f696e8ed471676f
NomadBlacky <tkadowaki.dev.blackey@gmail.com>
2019-03-04 21:52:40 +0900

    Add `Markers` examples

--- src/main/scala/Example.scala
+++ src/main/scala/Example.scala
@@ -1,3 +1,4 @@
+import net.logstash.logback.marker.LogstashMarker
 import org.slf4j.LoggerFactory
 
 import scala.collection.JavaConverters._
@@ -9,6 +10,8 @@ object Example extends App {
   // ふつうのログ
   logger.info("Hello, Logging with JSON!")
 
+  println("\n=== StructuredArguments ===================================================\n")
+
   // StructuredArgument で動的にフィールドを追加
   import net.logstash.logback.argument.StructuredArguments._
   logger.info("StructuredArguments.value {}", value("KEY", "VALUE"))
@@ -17,7 +20,17 @@ object Example extends App {
   logger.info("StructuredArguments.array {}", array("array", "a", "b", "c"))
   logger.info("StructuredArguments.raw {}", raw("raw", """{"KEY":"VALUE"}"""))
 
+  println("\n=== Markers ===================================================\n")
+
   // Marker で動的にフィールドを追加
   import net.logstash.logback.marker.Markers._
-  logger.info(append("marker", "mkr"), "Markers.append")
+  logger.info(append("KEY", "VALUE"), "Markers.append")
+  logger.info(appendEntries(Map("k1" -> "v1", "k2" -> "v2").asJava), "Markers.appendEntries")
+  logger.info(appendArray("array", "a", "b", "c"), "Markers.appendArray")
+  logger.info(appendRaw("raw", """{"KEY":"VALUE"}"""), "Markers.appendRaw")
+
+  val marker: LogstashMarker =
+    append("KEY_A", "VALUE_A").and(append("KEY_B", "VALUE_B"))
+  logger.info(marker, "multiple markers")
+
 }
